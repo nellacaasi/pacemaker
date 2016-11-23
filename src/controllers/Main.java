13d9dc4db7a692c576928d585650d5ed8e969572
Isaac Allen <nellacaasi@gmail.com>
2016-11-23 10:13:56 +0000

    Finished TDD-04 Lab

--- src/controllers/Main.java
+++ src/controllers/Main.java
@@ -1,15 +1,30 @@
 package controllers;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.Collection;
 
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+
 import models.User;
+import utils.Serializer;
+import utils.XMLSerializer;
 
 public class Main
 {
-  public static void main(String[] args) throws IOException
+  public static void main(String[] args) throws Exception
   {    
-	  PacemakerAPI pacemakerAPI = new PacemakerAPI();
+	    File  datastore = new File("datastore3.xml");
+	    Serializer serializer = new XMLSerializer(datastore);
+
+	    PacemakerAPI pacemakerAPI = new PacemakerAPI(serializer);
+	    if (datastore.isFile())
+	    {
+	      pacemakerAPI.load();
+	    }
 
 	    pacemakerAPI.createUser("Bart", "Simpson",   "bart@simpson.com", "secret");
 	    pacemakerAPI.createUser("Homer", "Simpson",  "homer@simpson.com", "secret");
@@ -19,10 +34,8 @@ public class Main
 	    System.out.println(users);
 
 	    User homer = pacemakerAPI.getUserByEmail("homer@simpson.com");
-	    System.out.println(homer);
+	    pacemakerAPI.createActivity(homer.id, "walk", "tramore", 1000);
 
-	    pacemakerAPI.deleteUser(homer.id);
-	    users = pacemakerAPI.getUsers();
-	    System.out.println(users);
+	    pacemakerAPI.store();
   }
 }
